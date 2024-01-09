62d11cfc5f466c1e0974ce66869f550b742c3bda
Lewis Lawler <lewislawler98@gmail.com>
2024-01-09 13:04:56 +0000

    adds quit button functionality

--- tetris/src/components/board/board.jsx
+++ tetris/src/components/board/board.jsx
@@ -1,11 +1,19 @@
-import "./board.css"
+import "./board.css";
+import { useNavigate } from "react-router-dom";
 
 const Board = () => {
+  const navigate = useNavigate();
+
+  const handleQuitButtonClick = () => {
+    navigate("/");
+  };
   return (
     <div className="game-container">
       <div className="board"></div>
       <p className="score">Score: 0</p>
-      <button className="quit-button">Quit</button>
+      <button className="quit-button" onClick={handleQuitButtonClick}>
+        Quit
+      </button>
     </div>
   );
 };
