9e1ddc389fb5944620a40893f96f7c7322d944ec
Lewis Lawler <lewislawler98@gmail.com>
2024-01-09 13:01:19 +0000

    adds css template for /game

--- tetris/src/components/board/board.jsx
+++ tetris/src/components/board/board.jsx
@@ -4,6 +4,7 @@ const Board = () => {
   return (
     <div className="game-container">
       <div className="board"></div>
+      <p className="score">Score: 0</p>
       <button className="quit-button">Quit</button>
     </div>
   );
