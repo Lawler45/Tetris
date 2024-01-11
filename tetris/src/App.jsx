abfb4276a27531543fd003742be2c85b87bdd6af
Lewis Lawler <lewislawler98@gmail.com>
2024-01-11 16:54:25 +0000

    adds start button and gameover hook

--- tetris/src/App.jsx
+++ tetris/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app">
       <StartPage />
-      <Board />
+      <Board rows={20} columns={10} />
     </div>
   );
 }
--- tetris/src/components/board/board.jsx
+++ tetris/src/components/board/board.jsx
@@ -1,11 +1,19 @@
 import "./board.css";
 import { useNavigate } from "react-router-dom";
+import { useGameOver } from "../../hooks/useGameOver";
 
-const Board = () => {
+const Board = ({ rows, columns }) => {
   const navigate = useNavigate();
 
   const handleQuitButtonClick = () => {
     navigate("/");
+  };
+
+  const [gameOver, setGameOver, resetGameOver] = useGameOver();
+
+  const start = () => {
+    resetGameOver();
+    console.log(`start gameOver is ${gameOver}`);
   };
   return (
     <div className="game-container">
@@ -13,6 +21,9 @@ const Board = () => {
       <p className="score">Score: 0</p>
       <button className="quit-button" onClick={handleQuitButtonClick}>
         Quit
+      </button>
+      <button className="start-button" onClick={start}>
+        Start
       </button>
     </div>
   );
