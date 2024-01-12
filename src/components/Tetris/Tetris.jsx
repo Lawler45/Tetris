4a953706dd3a45c70a51cd5d68117c245b2ddd19
Lewis Lawler <lewislawler98@gmail.com>
2024-01-12 18:25:45 +0000

    adds game stats skeleton

--- src/components/Tetris/Tetris.jsx
+++ src/components/Tetris/Tetris.jsx
@@ -1,11 +1,17 @@
 import "./Tetris.css";
 import Board from "../Board/Board.jsx";
-
-import useBoard  from "../../hooks/useBoard";
+import useBoard from "../../hooks/useBoard";
+import GameStats from "../GameStats/GameStats.jsx";
 
 const Tetris = ({ rows, columns, setGameOver }) => {
   const [board, setBoard] = useBoard({ rows, columns });
-  return <Board board={board} />;
+
+  return (
+    <div className="Tetris">
+      <Board board={board} />;
+      <GameStats gameStats={gameStats} />
+    </div>
+  );
 };
 
 export default Tetris;
